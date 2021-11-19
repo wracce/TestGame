98bb5095b7773600cb912a841dacac1cf7ad4a51
Wracce <wracce@gmail.com>
2021-11-19 14:14:03 +0400

    First commit

---
+++ cmake-build-debug/CMakeFiles/TestGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestGame.dir/src/main.cpp.obj"
+  "TestGame.exe"
+  "TestGame.exe.manifest"
+  "TestGame.pdb"
+  "libTestGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
